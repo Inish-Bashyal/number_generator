905ecdc220448f2b87f2b3978a070cf664939da6
Inish <inish.bashyal99@gmail.com>
2023-04-29 11:37:40 +0545

    app completed

--- lib/model/number_generator.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:math';
-
-class NumberGenerator {
-  int generate() {
-    var rn = Random();
-    int num = rn.nextInt(100);
-    return num;
-  }
-}
--- lib/view/dashboard_view.dart
+++ lib/view/dashboard_view.dart
@@ -1,5 +1,6 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:number_generator/model/number_generator.dart';
 
 class DashBoardView extends StatefulWidget {
   const DashBoardView({super.key});
@@ -9,12 +10,59 @@ class DashBoardView extends StatefulWidget {
 }
 
 class _DashBoardViewState extends State<DashBoardView> {
-  NumberGenerator ng = NumberGenerator();
+  int num1 = 0;
+  int num2 = 0;
+  int round = 0;
+  bool displayScore = false;
+  int wrongScore = 0;
+  int rightScore = 0;
+  void newRound() {
+    setState(() {
+      num1 = Random().nextInt(100);
+      num2 = Random().nextInt(100);
 
-  int numGen() {
-    ng.generate();
-    int number = ng.generate();
-    return number;
+      if (num1 != num2) {
+        round++;
+        displayScore = false;
+      } else {
+        num1 = Random().nextInt(100);
+        num2 = Random().nextInt(100);
+        round++;
+        displayScore = false;
+      }
+    });
+  }
+
+  void selectNumber(int number) {
+    setState(() {
+      if (num1 > num2 && number == 1) {
+        rightScore++;
+      } else if (num2 > num1 && number == 2) {
+        rightScore++;
+      } else {
+        wrongScore++;
+      }
+      if (round == 10) {
+        displayScore = true;
+      } else {
+        newRound();
+      }
+    });
+  }
+
+  void reset() {
+    setState(() {
+      round = 0;
+      wrongScore = 0;
+      rightScore = 0;
+      newRound();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    newRound();
   }
 
   @override
@@ -29,76 +77,87 @@ class _DashBoardViewState extends State<DashBoardView> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(
-            12,
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              RichText(
-                text: const TextSpan(
-                  text: 'Pick the Greatest Number',
-                  style: TextStyle(
-                    color: Colors.redAccent,
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Row(
+      body: Center(
+        child: displayScore
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(
-                    height: 200,
-                    width: 100,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red,
-                      ),
-                      child: Text(
-                        numGen().toString(),
-                        style: const TextStyle(
-                          fontSize: 22,
-                        ),
+                  const Text(
+                    'Final Score:',
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  Text(
+                    'Right Answer : $rightScore',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  Text(
+                    'Wrong Answer : $wrongScore',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: reset,
+                    child: const Text('Play Again'),
+                  ),
+                ],
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RichText(
+                    text: const TextSpan(
+                      text: 'Pick the Greatest Number',
+                      style: TextStyle(
+                        color: Colors.redAccent,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                  const Spacer(),
-                  SizedBox(
-                    height: 200,
-                    width: 100,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.red,
-                      ),
-                      child: Text(
-                        numGen().toString(),
-                        style: const TextStyle(
-                          fontSize: 22,
+                  Text(
+                    'Round $round',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 200,
+                        width: 100,
+                        child: ElevatedButton(
+                          onPressed: () => selectNumber(1),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                          ),
+                          child: Text(
+                            '$num1',
+                            style: const TextStyle(
+                              fontSize: 22,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
+                      SizedBox(
+                        height: 200,
+                        width: 100,
+                        child: ElevatedButton(
+                          onPressed: () => selectNumber(2),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.red,
+                          ),
+                          child: Text(
+                            '$num2',
+                            style: const TextStyle(
+                              fontSize: 22,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-              Container(
-                color: Colors.yellow,
-                // child: const Text('Wrong Answer'),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text('Right Answer'),
-                    Text('Wrong Answer'),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
